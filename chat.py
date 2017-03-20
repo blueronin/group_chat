e148217c5de20441dc0b4b4c760420410bb84f47
apipemc <andrepipem@gmail.com>
2017-03-20 17:17:40 -0500

    Add port and enable iptables

--- chat.py
+++ chat.py
@@ -1,7 +1,10 @@
 #!/bin/env python
+import os
 from app import create_app, socketio
 
 app = create_app(debug=True)
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 80))
+    app.run(host='0.0.0.0', port=port)
     socketio.run(app)
