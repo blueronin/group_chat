7ec8f4f2b0b42b87d6838d593311269d6d04cf44
safhl817 <safhl817@gmail.com>
2017-03-20 16:59:25 -0500

    BLL Layer created in .NET

---
+++ BLL/Mensaje.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace BLL
+{
+    public class Mensaje
+    {
+        //Propiedades
+        private int idMensaje;
+        private string nickname;
+        private string descripcion;
+
+        public int IdMensaje 
+        {
+            get { return idMensaje; }
+            set { idMensaje = value; }
+        }
+
+        public string Nickname
+        {
+            get { return nickname; }
+            set { nickname = value; }
+        }
+
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+
+        //Métodos
+
+        public List<Mensaje> GetMensajes()
+        {
+            List<BLL.Mensaje> mensajes = new List<BLL.Mensaje>();
+            DAL.Mensaje mensajeDAL = new DAL.Mensaje();
+            DataTable tableResult = mensajeDAL.GetMensajes();
+            foreach (DataRow item in tableResult.Rows)
+            {
+                Mensaje mensaje = new Mensaje();
+                mensaje.IdMensaje = int.Parse(item["idMensaje"].ToString());
+                mensaje.Nickname = item["nickname"].ToString();
+                mensaje.Descripcion = item["mensaje"].ToString();
+                mensajes.Add(mensaje);
+            }
+            return mensajes;
+        }
+
+        public void InsertMensaje(string mensaje, int idUsuario)
+        {
+            DAL.Mensaje mensajeDAL = new DAL.Mensaje();
+            mensajeDAL.InsertMensaje(mensaje, idUsuario);
+        }
+    }
+}
---
+++ BLL/Usuario.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace BLL
+{
+    public class Usuario
+    {
+        //Propiedades       
+
+        private int idUsuario;
+        private string nickname;
+        private string password;
+
+        public int IdUsuario
+        {
+            get { return idUsuario; }
+            set { idUsuario = value; }
+        }
+
+        public string Nickname
+        {
+            get { return nickname; }
+            set { nickname = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        //Métodos
+
+        public Usuario LoginUsuario(string nickname)
+        {
+            try
+            {
+                Usuario usuario = new Usuario();
+                DAL.Usuario usuarioDAL = new DAL.Usuario();
+                DataTable tableResult = usuarioDAL.LoginUsuario(nickname);
+                foreach (DataRow item in tableResult.Rows)
+                {
+                    usuario.IdUsuario = int.Parse(item["idUsuario"].ToString());
+                    usuario.Nickname = item["nickname"].ToString();                   
+                }
+                return usuario;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public List<Usuario> GetUsuarios()
+        {
+            List<BLL.Usuario> usuarios = new List<BLL.Usuario>();
+            DAL.Usuario mensajeDAL = new DAL.Usuario();
+            DataTable tableResult = mensajeDAL.GetUsuarios();
+            foreach (DataRow item in tableResult.Rows)
+            {
+                Usuario usuario = new Usuario();
+                usuario.IdUsuario = int.Parse(item["idUsuario"].ToString());
+                usuario.Nickname = item["nickname"].ToString();
+                usuarios.Add(usuario);
+            }
+            return usuarios;
+        }
+    }
+}
